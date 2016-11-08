6cb12502a344dae5cf0014fe8018a570b6d6bbb0
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-08 22:32:16 +0000

    pause button and play button added and working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
-
 $(document).ready(function() {
 
-);
+  if (!document.createElement('video').canPlayType) {
+    $("audio_controls").hide();
+    return;
+  }
+
+  var video = document.getElementById("audio");
+
+  $("#play_button").bind("click", function() {
+    audio.play();
+  });
+
+  $("#pause_button").bind("click", function() {
+    audio.pause();
+  });
+});

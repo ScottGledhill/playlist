8330062e92784bf2f9b3da5c9fd3696287923ee6
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-10 14:40:04 +0000

    synced the trackurl to the track class, and finally worked out how to make the audio player also change

--- src/app.js
+++ src/app.js
@@ -8,10 +8,24 @@ $(document).ready(function() {
     playlist.tracks.forEach(function (track) {
       list.push({title: track.title, url: track.stream_url});
     });
+
     for(i=0; i < list.length; i++) {
-      $('#title').append('<ul>'+list[i].title+'</ul>');
+      $('#title').append(
+        "<ul class='track-title' id='" +i+ "'>" +
+          list[i].title +
+        "</ul>"
+      );
     }
-    
+
+    $('.track-title').click(function(val) {
+      var audio = $("#audio");
+      var track = val.target.id;
+      var combinedUrl = list[track].url + "?client_id=8e74002fd2542f89231c5133c2a54833";
+      $("#player").attr("src", combinedUrl);
+      audio[0].pause();
+      audio[0].load();
+      audio[0].oncanplaythrough = audio[0].play();
+    });
   });
 
   var audio = document.getElementById("audio");
